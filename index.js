de4bea8f61cd3cf6a7912352442ddaf573b93b89
Aditya Kumar Jha <adityajha126@gmail.com>
2019-08-24 18:41:34 +0530

    added routes

--- index.js
+++ index.js
@@ -10,12 +10,30 @@ const request = require('request-promise');
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products';
-const forwardingAddress = "{ngrok forwarding address}"; // Replace this with your HTTPS Forwarding address
-
+const forwardingAddress = "https://7c8a6813.ngrok.io"; 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
+});
+
+//routes
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
 });
